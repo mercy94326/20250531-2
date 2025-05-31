0678b370fc49a37e79cb2cad755a438634542f39
mercy94326 <mercy94326@gmail.com>
2025-05-31 22:09:14 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -27,9 +27,9 @@ let questionSet = [
 ];
 
 function preload() {
-  fruitImages['watermelon'] = loadImage('pngtree-cute-anthropomorphic-fruit-watermelon-png-image_2844683-removebg-preview.png');
+  fruitImages['watermelon'] = loadImage('pngtree-cute-anthropomorphic-fruit-watermelon-png-image_2844683-removebg-preview');
   fruitImages['watermelon_half'] = loadImage('b248b63a1961e1f38d33f42e2b10066a-removebg-preview.png');
-  bombImg = loadImage('hpngtree-ignite-the-bomb-image_2233752-removebg-preview.png');
+  bombImg = loadImage('pngtree-ignite-the-bomb-image_2233752-removebg-preview.png');
 }
 
 function setup() {
